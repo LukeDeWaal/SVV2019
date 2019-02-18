eacee45002ee36975de8f7b8a419353a6ce29911
gianlucamancini7 <gianlucamancini7@gmail.com>
2019-02-18 16:53:14 +0100

    validation_data_exploration and shear flow

---
+++ shear_flow_cross_section.py
@@ -0,0 +1,28 @@
+'''Gianluca Mancini'''
+
+
+# here necessary imports
+import numpy as np
+
+
+def basic_shear_flow(v_z, v_y, cross_sectional_property):
+
+    ''' basic_shear_flow takes as input the
+    shear forces in the z and y direction
+     acting at the hinge in any point along the aileron
+
+     The coordinate frame system for this calculations tilted at
+    theta degrees with respect to the horizon line
+
+
+     v_z = shear force in the z direction
+     v_y = shear force in y direction
+     booms = booms objects
+     skin = skin objects
+     izz = moment of inertia around z axis
+     iyy = moment of inertia around y axis
+     '''
+
+    q_b=
+
+
