ff7dc37ea07eec9ad9fdbba96ea1631b0449dc1f
Luke Rijk de Waal <l.r.dewaal@student.tudelft.nl>
2019-02-18 13:09:28 +0100

    Assembling Structure

--- src/Structure.py
+++ src/Structure.py
@@ -6,43 +6,76 @@ from src.Idealizations import *
 import matplotlib.pyplot as plt
 import unittest
 import numpy as np
+from src.NumericalTools import newtons_method, derive
 
 ha = 0.205
 Ca = 0.605
 pi = np.pi
+h_stringer = 1.6*10**(-2)
 
 
-def plot_aileron(c, h):
+def get_crossectional_coordinates(c, h, hs):
+    """
+    Function to calculate a possible solution for evenly spaced stiffeners
+    :param c: Chordlength
+    :param h: Height
+    :param hs: Stiffener Height
+    :return: Array of Coordinates
+    """
     curved_coordinates = []
     straight_coordinates = []
 
-    #curved part
-    for theta in np.arange(pi/2+pi/4, 3*pi/2, pi/4):
-        curved_coordinates.append((None, np.sin(theta)*h/2, np.cos(theta)*h/2+h/2))
+    def curved_part(theta):
+        return [h/2*(1-np.sin(theta)), h/2*np.cos(theta)]
 
-    #straight part
     def lower_slope(x):
         return (h/2)/(c-h/2)*x - h/2
 
     def upper_slope(x):
         return -(h/2)/(c-h/2)*x + h/2
 
-    for slope in [lower_slope, upper_slope]:
-        for x in np.arange(h/2, c, (c-h/2)/6):
-            straight_coordinates.append((None, slope(x), x))
+    #Curved Part Cooprdinates
+    for theta in np.arange(pi/4,pi, pi/4):
+        curved_coordinates.append(curved_part(theta))
 
-    return curved_coordinates+straight_coordinates
+    #Need to check what the maximum distance is untill stiffeneres dont fit anymore
+    angle = np.arctan((h/2)/(c-h/2))   #Stiffener angle with vertical
+    max_height_diff = hs*np.cos(angle) #Vertical height of stiffener
 
-a = np.array(plot_aileron(Ca, ha))
+    max_x_1 = newtons_method(lambda x: upper_slope(x)-max_height_diff, c)
+    max_x_2 = newtons_method(lambda x: lower_slope(x)+max_height_diff, c)
+    if round(max_x_1, 8) == round(max_x_2, 8):
+        print(max_x_1)
+    else:
+        return
 
-plt.scatter(a[:,2], a[:,1])
+    x_range = np.linspace(h/2, max_x_1, 7)
+    for x in x_range[1:]:
+        straight_coordinates.append([x, lower_slope(x)])
+
+    for x in x_range[1:][::-1]:
+        straight_coordinates.append([x, upper_slope(x)])
+
+    array = np.array(curved_coordinates+straight_coordinates)
+
+    return np.concatenate((np.zeros((len(array), 1)), array), axis=1)
+
+
+def plot_crosssection(positions):
+
+    plt.scatter(positions[:,0], positions[:,1])
+    plt.axes().set_aspect('equal')
+    plt.grid(True)
+
 
 class CrossSection(object):
 
-    def __init__(self, *objects: "Booms and/or Skin"):
+    def __init__(self, coordinates, x_coordinate=0):
 
-        self.__objects = objects
-        self.__structure = None
+        self.__coordinates = coordinates
+        self.__coordinates[:,0] += x_coordinate
+
+
 
 
 class Structure(object):
@@ -58,7 +91,7 @@ if __name__ == "__main__":
 
         def setUp(self):
 
-            self.structure = CrossSection()
+            self.CS = CrossSection(a, 1)
 
 
     def run_TestCases():
